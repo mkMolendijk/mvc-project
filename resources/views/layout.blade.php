d41cd55730a41b1cc53a1535ded266bf49d2b939
mkMolendijk <markmolendijk@Marks-MacBook-Pro.local>
2016-10-06 11:56:30 +0200

    Edited layout file and added build folder to gitignore

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
     @yield('head')
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link href="{{ URL::asset('sass/app.sass') }}" rel="stylesheet" type="text/css">
+    <link href="{{ elixir ('css/app.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
 
