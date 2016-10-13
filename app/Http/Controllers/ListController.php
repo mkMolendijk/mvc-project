89c6e76aab79e0dcd918bf2dd6960bb7e2109f33
mkMolendijk <markmolendijk@Marks-MacBook-Pro.local>
2016-10-13 11:22:04 +0200

    Added controllers for the lists and items

---
+++ app/Http/Controllers/ListController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ListController extends Controller
+{
+    public function index()
+    {
+        return view('lists.index');
+    }
+
+    public function addList(){
+        //
+    }
+}
